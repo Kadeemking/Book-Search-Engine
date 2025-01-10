47d5872154cc0c743ab4ce88be449a78977f647a
kadeem <kingkadeem11@gmail.com>
2025-01-10 12:52:12 -0500

    added apollo server to server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const { ApolloServer }= require('@apollo/server');
+const { expressMiddleware }= require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server= new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+const startApolloServer= async () => {
+  await server.start();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('graphql', expressMiddleware(server, {
+  context: authMiddleware
+}));
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -17,5 +31,12 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on http://localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
+  });
 });
+}
+
+// Call the async function to start the server
+startApolloServer();
